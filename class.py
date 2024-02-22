94ef639be8094a9f03d09f4d50c25ddd295a9dbb
NasrinNasrin <144053284+NasrinNasrin@users.noreply.github.com>
2024-02-22 20:24:07 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,16 @@
+class Student:
+    name = ""
+    age =0
+student1=Student()
+student2=Student()
+
+student1.name="aswathy" 
+student2.name="akhil"   
+print(student1.name)
+print(student2.name)
+
+student1.age=20
+student2.age=19
+print(student1.age)
+print(student2.age)
+
