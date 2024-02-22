956c863e9423ce2b5fc893876e074e634d3b2365
NasrinNasrin <144053284+NasrinNasrin@users.noreply.github.com>
2024-02-22 20:25:18 +0530

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,26 @@
+class Animal:
+
+    name =""
+    leg=0
+
+
+dog= Animal() 
+cat=Animal() 
+elephant=Animal()
+
+
+dog.name="Lucky"
+cat.name="Roma"
+elephant.name="Lumpy"
+print(dog.name)
+print(cat.name)
+print(elephant.name)
+
+
+dog.leg="4"
+cat.leg="4"
+elephant.leg="4"
+
+print(dog.leg)
+print(cat.leg)
+print(elephant.leg)
